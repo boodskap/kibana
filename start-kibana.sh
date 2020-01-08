cb185f622a5071dc9f45c7ffeb724d5807323894
Jegan Vincent <jegan@abcs-MacBook-Pro-4.local>
2020-01-08 11:47:35 +0530

    fixed ownership

--- start-kibana.sh
+++ start-kibana.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "Fixing data dir ownership"
+
+sudo chown -R kibana:kibana /home/kibana/data
+
 su - kibana /home/kibana/bin/kibana
 
 /bin/bash -c "trap : TERM INT; sleep infinity & wait"
